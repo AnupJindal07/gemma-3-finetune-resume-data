94b372905e53d4468e63dae81c0de127db54f662
Anup Jindal <anup.jindal@sap.com>
2026-01-23 19:02:25 +0530

    force install

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ echo "Setting up Resume Analyzer AI..."
 mkdir -p data/raw_data data/processed_data models/checkpoints
 
 # Install dependencies
-pip install -r requirements.txt
+#pip install -r requirements.txt
+pip install --index-url https://pypi.org/simple --extra-index-url https://download.pytorch.org/whl/cu121 -r requirements.txt
 
 echo "Setup complete!"
